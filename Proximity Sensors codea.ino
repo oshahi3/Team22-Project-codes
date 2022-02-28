00c2de6aaeb435174b7e931c2e87a6c64c4c137e
oshahi3 <78010214+oshahi3@users.noreply.github.com>
2022-02-27 19:02:51 -0600

    Update Proximity Sensors codea.ino

--- Proximity Sensors codea.ino
+++ Proximity Sensors codea.ino
@@ -1,5 +1,25 @@
+#include <Wire.h>
+
 int Data_Bus = 3;
 int clock = 4;
+
+
+
+void setup() {
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
+}
+
+void loop() {
+  Wire.requestFrom(8, 6);    // request 6 bytes from slave device #8
+
+  while (Wire.available()) { // slave may send less than requested
+    char c = Wire.read(); // receive a byte as character
+    Serial.println(c);         // print the character
+  }
+
+  delay(500);
+}
 
 void setup() {
   // put your setup code here, to run once:
