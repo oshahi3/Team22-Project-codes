f917c86418830bffe6c820a2272881f413cf3f55
oshahi3 <78010214+oshahi3@users.noreply.github.com>
2022-03-05 14:53:02 -0600

    Update Accelerometer.ino

--- Accelerometer.ino
+++ Accelerometer.ino
@@ -1,12 +1,33 @@
 // code for accelerometer 
 
-#include <Wire.h> 
+const int xpin = A3;                  // x-axis of the accelerometer
+const int ypin = A2;                  // y-axis
+const int zpin = A1;                  // z-axis 
+int xVal, yVal, zVal;
 void setup() {
-  // put your setup code here, to run once:
-
+  // initialize the serial communications:
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ xVal = digitalRead(xpin);
+ int x = map(xVal, "%d", "%d", "%d", "%d");
+ float xg = (float)x/(-100.00);
+ Serial.print(xg);
+ Serial.print("g     ");
 
+ yVal = digitalRead(ypin);
+ int y = map(yVal, "%d", "%d", "%d", "%d");
+ float yg = (float)y/(-100.00);
+ Serial.print("\t");
+ Serial.print(yg);
+ Serial.print("g     ");
+
+zVal = digitalRead(zpin);
+int z = map(zVal, "%d", "%d", "%d", "%d");
+float zg = (float)z/(-100.00);
+Serial.print("\t");
+Serial.print(zg);
+Serial.println("g     ");  
+  delay(100);
 }
