c460fc525eb175e326a009b856014fcbf2d26c1b
oshahi3 <78010214+oshahi3@users.noreply.github.com>
2022-03-07 11:24:58 -0600

    Update Proximity Sensors codea.ino

--- Proximity Sensors codea.ino
+++ Proximity Sensors codea.ino
@@ -1,8 +1,9 @@
+#include <avr/io.h>
+#include <util/delay.h>
 #include <Wire.h>
 
 int Data_Bus = 3;
 int clock = 4;
-
 
 
 void setup() {
@@ -11,7 +12,7 @@ void setup() {
 }
 
 void loop() {
-  Wire.requestFrom(8, 6);    // request 6 bytes from slave device #8
+  Wire.requestFrom(8, 7);    // request 6 bytes from slave device #7
 
   while (Wire.available()) { // slave may send less than requested
     char c = Wire.read(); // receive a byte as character
@@ -20,16 +21,16 @@ void loop() {
 
   delay(500);
 }
+//
+//void setup() {
+//  // put your setup code here, to run once:
+//  pinMode(Data_Bus, OUTPUT);
+//
+//}
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(Data_Bus, OUTPUT);
-
-}
-
-void loop() {
-  digitalWrite(clock, HIGH);
-  delay(1000);  
-  digitalWrite(Data_Bus, LOW); 
-  delay(1000);
-}
+//void loop() {
+//  digitalWrite(clock, HIGH);
+//  delay(1000);  
+//  digitalWrite(Data_Bus, LOW); 
+//  delay(1000);
+//}
