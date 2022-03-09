4162843cf909c37e7fe25605d27d75c1eae7e1dc
oshahi3 <78010214+oshahi3@users.noreply.github.com>
2022-03-09 17:50:10 -0600

    Update Accelerometer.ino

--- Accelerometer.ino
+++ Accelerometer.ino
@@ -1,33 +1,39 @@
 // code for accelerometer 
 
-const int xpin = A3;                  // x-axis of the accelerometer
-const int ypin = A2;                  // y-axis
-const int zpin = A1;                  // z-axis 
-int xVal, yVal, zVal;
-void setup() {
-  // initialize the serial communications:
-  Serial.begin(9600);
+int xpin = A3;                
+ int ypin = A2;                
+ int zpin = A1; 
+ int xvalue;
+ int yvalue;
+ int zvalue;
+
+void setup()
+{
+   Serial.begin(9600);          // initialize the serial communications:
 }
 
-void loop() {
- xVal = digitalRead(xpin);
- int x = map(xVal, "%d", "%d", "%d", "%d");
- float xg = (float)x/(-100.00);
- Serial.print(xg);
- Serial.print("g     ");
 
- yVal = digitalRead(ypin);
- int y = map(yVal, "%d", "%d", "%d", "%d");
- float yg = (float)y/(-100.00);
- Serial.print("\t");
- Serial.print(yg);
- Serial.print("g     ");
-
-zVal = digitalRead(zpin);
-int z = map(zVal, "%d", "%d", "%d", "%d");
-float zg = (float)z/(-100.00);
-Serial.print("\t");
-Serial.print(zg);
-Serial.println("g     ");  
+void loop()
+{
+  xvalue = analogRead(xpin);                              //reads values from x-pin & measures acceleration in X direction 
+  int x = map(xvalue, 267, 400, -100, 100);               //maps the extreme ends analog values from -100 to 100 for our understanding
+//ou need to replace the 267 & 400 value with your values from calibration
+  float xg = (float)x/(-100.00);                          //converts the mapped value into acceleration in terms of "g"
+  Serial.print(xg);                                       //prints value of acceleration in X direction
+  Serial.print("g   ");                                   //prints "g"
+   
+  yvalue = analogRead(ypin);
+  int y = map(yvalue, 272, 406, -100, 100);
+  float yg = (float)y/(-100.00);
+  Serial.print("\t");
+  Serial.print(yg);
+  Serial.print("g   "); 
+ 
+  zvalue = analogRead(zpin);
+  int z = map(zvalue, 277, 410, -100, 100);
+  float zg = (float)z/(100.00);
+  Serial.print("\t"); 
+  Serial.print(zg);
+  Serial.println("g   ");
   delay(100);
 }
